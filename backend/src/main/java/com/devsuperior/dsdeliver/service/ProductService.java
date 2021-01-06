18834af25e39c2c8edbd12a9dde3926835b407ce
unknown <lazaronobrega3@gmail.com>
2021-01-06 09:31:25 -0200

    GET /products

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dsdeliver.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdeliver.dto.ProductDTO;
+import com.devsuperior.dsdeliver.entites.Product;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<ProductDTO> findAll(){
+		
+		List<Product> list = repository.findAllByOrderByName();
+		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+		
+	}
+
+}
