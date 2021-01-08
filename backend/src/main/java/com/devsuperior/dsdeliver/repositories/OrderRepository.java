10c057ca057783f1a7dc185f455a8dc8a0caf382
unknown <lazaronobrega3@gmail.com>
2021-01-08 16:06:31 -0200

    C:/Program Files (x86)/Git/oreders

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
@@ -1,9 +1,16 @@
 package com.devsuperior.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsdeliver.entites.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long>{
-
+		
+	//@Query("select obj from Order obj JOIN FETCH obj.products
+	//@Query("select obj from Order obj WHERE obj.status = 0 obj.moment ASC")
+	//List<Order> findOrderWhithProducts();
+	
 }
